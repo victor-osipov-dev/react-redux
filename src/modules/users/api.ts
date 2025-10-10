97223244d34cd62e23b3566544302c04d789d8ad
Victor Osipov <victorosipovprogrammer@gmail.com>
2025-10-10 13:19:15 +0300

    add zod in transformResponse

--- src/modules/users/api.ts
+++ src/modules/users/api.ts
@@ -1,27 +1,29 @@
+import z from "zod";
 import { baseApi } from "../../shared/api";
 import type { User, UserId } from "./users.slice";
+
+const UserDtoSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    description: z.string(),
+});
 
 export const usersApi = baseApi.injectEndpoints({
     endpoints: (create) => ({
         getUsers: create.query<User[], void>({
             query: () => "/users",
-            providesTags: ["Users", { type: "Users", id: "LIST" }],
+            providesTags: ["Users"],
+            transformResponse: (res: unknown) =>
+                UserDtoSchema.array().parse(res),
         }),
         getUser: create.query<User, UserId>({
             query: (userId) => "/users/" + userId,
-            providesTags: (_, __, userId) => [
-                "Users",
-                { type: "Users", id: userId },
-            ],
+            providesTags: ["Users"],
+            transformResponse: (res: unknown) => UserDtoSchema.parse(res),
         }),
         deleteUser: create.mutation<void, UserId>({
             query: (userId) => ({ method: "DELETE", url: `/users/${userId}` }),
-            invalidatesTags: (_, __, userId) => {
-                return [
-                    { type: "Users", id: "LIST" },
-                    { type: "Users", id: userId },
-                ];
-            },
+            invalidatesTags: ["Users"],
         }),
     }),
     overrideExisting: true,
